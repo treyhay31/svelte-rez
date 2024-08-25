d86b78e405b050fd436cdb36cdde053f30043c39
Trey Hayden <treyhay@gmail.com>
2024-08-24 23:11:35 -0400

    profile pics & scroll animations

--- src/App.svelte
+++ src/App.svelte
@@ -39,13 +39,15 @@
     work: [
       {
         position: "Lead Software Engineer",
-        company: "Elavon, Inc.",
-        startDate: "2019-04-01",
-        endDate: "2024-08-01",
+        company: "U.S. Bank",
+        startDate: "2019",
+        endDate: "now",
         summary:
-          "Part of an awesome team where we collaborate around complex problems and work tirelessly to solve them for the benefit of our customers in the payment space.",
+          "At the bank I've been involved with several different teams dealing with a broad range of tech. We support ecommerce, mobile apps, payment terminals, and most recently technology governance.",
         website: "https://www.elavon.com/index.html",
         highlights: [
+          "Created entire end-to-end functional test suite using Playwright, shrinking our testing from around 30 mins per cycle down to 1 min per cycle",
+          "Handled requirements gathering to production deployment for features in our governance app which increases compliance measures and decreases time to production for 1,000s of developers",
           "Led team that placed in the top 5 for our company's internal MS Teams Application hackathon",
           "Led team of 10 developers on new secret management using Hashicorp Vault for wide range of applications",
           "Enhanced & maintained applications that support payment processing across windows, web, iOS, and Android platorms",
@@ -59,8 +61,8 @@
       {
         position: "Senior Consultant",
         company: "CapTech Ventures, Inc",
-        startDate: "2019-01-01",
-        endDate: "2019-04-01",
+        startDate: "2019",
+        endDate: "2019",
         summary:
           "Our team helped bring a cloud solution and proper development best practices to clients of all levels of expertise.",
         website: "https://www.captechconsulting.com/",
@@ -73,8 +75,8 @@
       {
         position: "Software Developer",
         company: "Dimensional Fund Advisors",
-        startDate: "2017-04-01",
-        endDate: "2019-01-01",
+        startDate: "2017",
+        endDate: "2019",
         summary:
           "Our team handled many internal applications for the portfolio managers and traders. On top of addressing bugs and feature requests, we automated integration pipelines and greatly shrank the level of effort required for deployments.",
         website: "https://www.dimensional.com/",
@@ -87,26 +89,26 @@
       {
         position: "Software Developer",
         company: "ETS: Electronic Transaction Systems",
-        startDate: "2013-06-01",
-        endDate: "2017-03-01",
+        startDate: "2013",
+        endDate: "2017",
         summary:
           "Our development team developed online checkout solutions and peer to peer payments for customers.",
         website: "https://elavonpayments.com/",
         highlights: [
-          "Developed Web API for company peer-to-peer payment platform",
+          "Developed & maintained Web API for company peer-to-peer payment platform",
           "Developed SDK for company consumer-to-business payment platform",
         ],
       },
       {
         position: "Consultant",
         company: "Deloitte",
-        startDate: "2011-08-01",
-        endDate: "2013-06-01",
+        startDate: "2011",
+        endDate: "2013",
         summary:
-          "As part of the Audit, Valuation, & Analytics (AVA) group our team helped clients organize and analyze their data using Excel, VBA, and SQL.",
+          "As part of the Audit, Valuation, & Analytics (AVA) group our team helped clients organize and analyze their data.",
         website: "https://www2.deloitte.com/us/en.html",
         highlights: [
-          "Aided in audit readiness using MS Excel, VBA, SAS, and ACL",
+          "Used tools: MS Excel, VBA, SAS, and ACL to bring reconcile data and bring insights to clients",
         ],
       },
     ],
@@ -159,59 +161,107 @@
 </script>
 
 <main>
+  <section id="sticky-parallax-header">
+    <h1>
+      Hi, I'm {rez.basics.name}.
+      <br />
+      No, the honor is all mine!
+    </h1>
+  </section>
+  <section class="work">
+    <h2>
+      Here is the stuff I did... (and still do but just soOoOo much better now!)
+    </h2>
+    <ul id="cards">
+      {#each rez.work as job, index}
+        <li class="card" id="card_{index}">
+          <article>
+            <h3>{job.position}</h3>
+            <h4>{job.company}</h4>
+            <p>{job.startDate} - {job.endDate}</p>
+            <p>{job.summary}</p>
+            <ul>
+              {#each job.highlights as highlight}
+                <li>{highlight}</li>
+              {/each}
+            </ul>
+          </article>
+        </li>
+      {/each}
+    </ul>
+  </section>
   <section class="me">
-    <h1>Hi, I'm {rez.basics.name}. No, the honor is all mine!</h1>
     <h2>{rez.basics.label}</h2>
     <h2>{rez.basics.email}</h2>
     <h2>{rez.basics.phone}</h2>
   </section>
-  <section class="work">
-    <h2>Here is the stuff I did... and still do (only I am better now)</h2>
-    {#each rez.work as job}
-      <article>
-        <h3>{job.position}</h3>
-        <h4>{job.company}</h4>
-        <p>{job.startDate} - {job.endDate}</p>
-        <p>{job.summary}</p>
-        <ul>
-          {#each job.highlights as highlight}
-            <li>{highlight}</li>
-          {/each}
-        </ul>
-      </article>
-    {/each}
-  </section>
 </main>
 
 <style>
+  @keyframes sticky-parallax-header-move-and-size {
+    from {
+      background-position: 50% 0;
+      height: 100vh;
+      font-size: calc(4vw + 1em);
+    }
+    to {
+      background-position: 50% 100%;
+      background-color: #0b1584;
+      color: #fff;
+      height: 13vh;
+      font-size: 1rem;
+    }
+  }
+
+  #sticky-parallax-header {
+    position: fixed;
+    top: 0;
+    width: 100vw;
+
+    animation: sticky-parallax-header-move-and-size linear forwards;
+    animation-timeline: scroll();
+    animation-range: 0vh 90vh;
+  }
+
+  .card {
+    --index0: calc(var(--index) - 1); /* 0-based index */
+    --reverse-index: calc(var(--numcards) - var(--index0)); /* reverse index */
+    --reverse-index0: calc(
+      var(--reverse-index) - 1
+    ); /* 0-based reverse index */
+  }
+
+  @keyframes scale {
+    to {
+      transform: scale(calc(1.1 - calc(0.1 * var(--reverse-index))));
+    }
+  }
+
+  #cards {
+    --numcards: 4;
+    view-timeline-name: --cards-element-scrolls-in-body;
+  }
+
+  .card__content {
+    --start-range: calc(var(--index0) / var(--numcards) * 100%);
+    --end-range: calc((var(--index)) / var(--numcards) * 100%);
+
+    animation: linear scale forwards;
+    animation-timeline: --cards-element-scrolls-in-body;
+    animation-range: exit-crossing var(--start-range) exit-crossing
+      var(--end-range);
+  }
   main {
+    padding-top: 133vh;
     font-family: Arial, sans-serif;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-
-  h1 {
-    color: #333;
-  }
-
-  h2 {
-    color: #666;
-  }
-
-  ul {
-    padding-left: 20px;
   }
 
   /* Print styles */
   @media print {
-    body {
+    main {
       font-size: 12pt;
       line-height: 1.5;
       color: #000;
-    }
-
-    main {
       max-width: 100%;
       padding: 0;
     }
